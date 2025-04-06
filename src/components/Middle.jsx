6f02d9b5560a2467b9a6d9e0b88ac8d054c4ab8e
Atulyajain306 <atulyajain306@gmail.com>
2025-04-07 02:43:38 +0530

    newest

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -3,17 +3,32 @@ import {Link} from "react-router-dom"
 import { PiConfettiBold } from "react-icons/pi";
 import { BsArrowRight } from "react-icons/bs";
 import { MdOutlineArrowBack } from "react-icons/md";
+import { useEffect,useState } from 'react';
+import { PiConfetti } from "react-icons/pi";
 import { useAuthContext } from '../context/Authcontext';
 const Middle = () => {
        const {setauthlang,setnewstate}=useAuthContext();
+       const [ww, setww] = useState(false);
         const Handleback=()=>{
               setauthlang(null);
               setnewstate(true);
         }
+      const change=()=>{
+          setww(!ww);
+        } 
+
+        useEffect(() => {
+          
+       setInterval(() => {
+        change()
+       }, 3000);   
+          
+        }, [ww])
+        
   return (
     <div className='container'>
         <div style={{marginTop:"50px"}} className="mood">MoodVEE</div>
-        <div className='register'>Sign Up <PiConfettiBold className='icons' /></div>
+        <div className='register'>Sign Up { ww ? <PiConfetti className="icons" /> : <PiConfettiBold className='icons' />}</div>
         <div className='register'>Successful</div>
         <div className='register'>Welcome to</div>
         <div className='register'>MoodVEE</div>
